c8e50eef98a7e2ff31cf4c8cbc0c4757904c19b2
David Daniel Gutiérrez Calderón <148672101+ddgutierrezc@users.noreply.github.com>
2024-09-15 14:13:14 -0600

    Update header.component.tsx

--- cafe-don-emilio/src/app/components/header/header.component.tsx
+++ cafe-don-emilio/src/app/components/header/header.component.tsx
@@ -1,6 +1,6 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCartShopping} from "@fortawesome/free-solid-svg-icons";
-import {PagesUrlsEnum} from "../../common/enums";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { PagesUrlsEnum } from "../../common/enums";
 
 /**
  * Component for the header of the application
@@ -13,42 +13,107 @@ export const HeaderComponent = () => {
                 <nav className="navbar navbar-expand-xl bg-color-medium px-4">
                     <div className="container-fluid">
                         <a className="navbar-brand" href={PagesUrlsEnum.HOME}>
-                            <img className="navbar-brand__logo" src="/logo.svg" alt="Logo de la empresa cafe don emilio"/>
+                            <img
+                                className="navbar-brand__logo"
+                                src="/logo.svg"
+                                alt="Logo de la empresa cafe don emilio"
+                            />
                         </a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                aria-expanded="false" aria-label="Toggle navigation">
+                        <button
+                            className="navbar-toggler d-xl-none" // Solo se muestra en lg y menores
+                            type="button"
+                            data-bs-toggle="offcanvas"
+                            data-bs-target="#offcanvasExample"
+                            aria-controls="offcanvasExample"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+                        <div className="collapse navbar-collapse d-none d-xl-flex"> {/* Se muestra solo en pantallas grandes */}
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link texto-sm-1 fw-normal active" aria-current="page" href={PagesUrlsEnum.OUR_HISTORY}>Nuestra historia</a>
+                                    <a
+                                        className="nav-link texto-sm-1 fw-normal active"
+                                        aria-current="page"
+                                        href={PagesUrlsEnum.OUR_HISTORY}
+                                    >
+                                        Nuestra historia
+                                    </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link texto-sm-1 fw-normal" href="#">Preguntas frecuentes</a>
+                                    <a className="nav-link texto-sm-1 fw-normal" href="#">
+                                        Preguntas frecuentes
+                                    </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link texto-sm-1 fw-normal" href="#">Tienda de café</a>
+                                    <a className="nav-link texto-sm-1 fw-normal" href="#">
+                                        Tienda de café
+                                    </a>
                                 </li>
                             </ul>
                             <div>
-                                <button className="btn btn-outline-primary me-3">
-                                    ES
-                                </button>
+                                <button className="btn btn-outline-primary me-3">ES</button>
 
                                 <button className="btn btn-md-imp btn-secondary color-light me-3 rounded-5">
                                     Reservar
                                 </button>
 
-                                <button className="btn btn-primary btn-sm-1-imp rounded-5 ">
+                                <button className="btn btn-primary btn-sm-1-imp rounded-5">
                                     <FontAwesomeIcon size={"xl"} icon={faCartShopping} />
                                 </button>
                             </div>
                         </div>
                     </div>
                 </nav>
+
+                {/* Inicio del offcanvas */}
+                <div
+                    className="offcanvas offcanvas-start bg-color-medium-imp d-xl-none" // Solo visible en pantallas lg y menores
+                    tabIndex={-1}
+                    id="offcanvasExample"
+                    aria-labelledby="offcanvasExampleLabel"
+                >
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title" id="offcanvasExampleLabel">
+                            Menú
+                        </h5>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-dismiss="offcanvas"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link texto-sm-1 fw-normal active" aria-current="page" href={PagesUrlsEnum.OUR_HISTORY}>
+                                    Nuestra historia
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link texto-sm-1 fw-normal" href="#">Preguntas frecuentes</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link texto-sm-1 fw-normal" href="#">Tienda de café</a>
+                            </li>
+                        </ul>
+                        <div className="my-3">
+                            <button className="btn btn-outline-primary me-3">ES</button>
+                            <button className="btn btn-primary btn-sm-1-imp rounded-5 ">
+                                <FontAwesomeIcon size={"xl"} icon={faCartShopping}/>
+                            </button>
+                        </div>
+                        <div className="text-center">
+                            <button className="btn btn-md-imp btn-secondary color-light me-3 rounded-5">Reservar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/* Fin del offcanvas */}
             </header>
         </>
-    )
-}
+    );
+};
